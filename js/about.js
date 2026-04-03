761687c884f0cc762d3c3fbb06d75b0efc50309a
LGNon <leegano0511@gmail.com>
2026-04-03 13:37:08 +0900

    v.Beta1.0

---
+++ js/about.js
@@ -0,0 +1,7 @@
+$(function() {
+    // about 페이지 전용 스크립트 (필요 시 작성)
+    console.log("About 페이지 로드 완료");
+    
+    // index에서 썼던 헤더 스크롤 로직이 .visual 클래스 기준으로 작동하므로
+    // .about_visual에도 .visual 클래스를 추가해두었습니다.
+});
--- js/script.js
+++ js/script.js
@@ -1,197 +1,163 @@
 $(function () {
 
-  // a태그 눌러도 최상단으로 이동 안하게하기
-  $(this).click(function(e){
-    e.preventDefault()
-  })
+  // [수정] 모든 a태그 이동 방지 대신, href="#"인 경우만 방지하도록 변경
+  // 이래야 로고(index.html)나 다른 페이지 이동이 정상 작동합니다.
+  $(document).on('click', 'a[href="#"]', function(e){
+    e.preventDefault();
+  });
 
   $(window).on('scroll load', function () {
+    const header = $('.header'); // common.css에 정의된 클래스 기준
+    const visual = $('.visual');
+    const visuLength = visual.length;
+    const visuHeight = visual.outerHeight();
+    const headerHeight = header.outerHeight();
 
-    const header = $('.header')
-    const visuLength = $('.visual').length;
-    const visuHeight = $('.visual').outerHeight();
-    const headerHeight = header.outerHeight()
+    let scrollTop = $(window).scrollTop();
 
-    let scrollTop = $(window).scrollTop()
-
-    header.addClass('load')
+    header.addClass('load');
 
     if (visuLength > 0) {
       if (scrollTop >= visuHeight - headerHeight) {
-        header.addClass('down')
+        header.addClass('down');
       } else {
-
-        header.removeClass('down')
+        header.removeClass('down');
       }
     }
+  });
 
-
-
-  })
-
-  allMenu()
-  visual() //함수의 호출
+  allMenu();
+  visual(); 
+  tap();
 
   function allMenu() {
-    const header = $('.header') //active
-    const hBtn = $('.btn_all_menu')
-    const mBtn = $('.depth1>li>span')
+    const header = $('.header');
+    const hBtn = $('.btn_all_menu');
+    const mBtn = $('.depth1 > li > span');
 
     // mobile btn click event
     mBtn.click(function(){
       if($(this).parent('li').hasClass('On')){
-        $(this).parent('li').removeClass('On')
-        $(this).siblings('ul.depth2').slideUp()
+        $(this).parent('li').removeClass('On');
+        $(this).siblings('ul.depth2').slideUp();
       }
       else{
-        $(this).parent('li').addClass('On').siblings().removeClass('On').find('ul.depth2').slideUp()
-        $(this).siblings('ul.depth2').slideDown()
+        $(this).parent('li').addClass('On').siblings().removeClass('On').find('ul.depth2').slideUp();
+        $(this).siblings('ul.depth2').slideDown();
       }
-    })
+    });
 
     // window resize
     $(window).on('load resize', function(){
-      if(window.innerWidth>1100){
-        mBtn.parent('li').removeClass('On')
-        mBtn.siblings('ul.depth2').removeAttr('style')
+      if(window.innerWidth > 1100){
+        mBtn.parent('li').removeClass('On');
+        mBtn.siblings('ul.depth2').removeAttr('style');
       }
-    })
+    });
 
-    //desktop  click event
+    // desktop click event (hamburger)
     hBtn.click(function (e) {
-      e.preventDefault()
-      if (header.hasClass('active') == false) {
-        header.addClass('active')
-      }
-      else {
-        header.removeClass('active')
-      }
-    })
+      e.preventDefault();
+      header.toggleClass('active');
+    });
   }
 
-  //0 visual 함수
   function visual() {
+    let slider = $('.visual .slider li');
+    let nav = $('.visu_nav li');
+    let sNum = slider.length;
+    if (sNum === 0) return; // 슬라이더가 없으면 함수 종료 (에러 방지)
 
-    // 1 키워드 정의
-    let slider = $('.visual .slider li') // slider   --> On
-    let nav = $('.visu_nav li') // visu_nav ---->Active
-    let sNum = slider.length //slider의 개수
-    let crt = 0
+    let crt = 0;
+    slFunc(crt);
+    let play = setInterval(visu, 5000);
 
-    slFunc(crt) //slider 함수의 호출
-    let play = setInterval(visu, 5000)
-
-    //2 slider 초기화 함수
     function slFunc(i) {
-
-      slider.eq(i).addClass('On')
-      nav.eq(i).addClass('Active')
+      slider.eq(i).addClass('On');
+      nav.eq(i).addClass('Active');
     }
 
-    // 3 visu nav click
-    nav.click(navFunc)
+    nav.click(function() {
+      let i = $(this).index();
+      reset();
+      slFunc(i);
+    });
 
-    // 4 nav function
-    function navFunc() {
-      let i = $(this).index()
-
-      reset()
-      slFunc(i)
-    }
-
-    //  6 slider 함수
     function visu() {
-
-      let idx = $('.visual .slider li.On').index()
-      console.log(idx);
+      let idx = $('.visual .slider li.On').index();
       idx++;
-      if (idx == sNum) {
-        idx = 0;
-      }
-
-      reset()
-      slider.eq(idx).addClass('On')
-      nav.eq(idx).addClass('Active')
+      if (idx == sNum) idx = 0;
+      reset();
+      slider.eq(idx).addClass('On');
+      nav.eq(idx).addClass('Active');
     }
 
-    // 5 reset function
     function reset() {
-      slider.removeClass('On')
-      nav.removeClass('Active')
+      slider.removeClass('On');
+      nav.removeClass('Active');
     }
-
   }
 
-  const ani = $('.ani')
-
-  $.fn.aniMoving = function () { //사용자 정의 메소드
-
-    let elementTop = $(this).offset().top; //선택한 요소의 Y축 좌표값
-    let elementBottom = elementTop + $(this).outerHeight(); //Y축 좌표값+현재요소의 높이값  
-
-    let viewportTop = $(window).scrollTop(); //화면의 스크롤값
-    let viewportBottom = viewportTop + $(window).height() //스크롤값+화면의 높이값
-
-    return (viewportTop < elementBottom) && (elementTop < viewportBottom)
-  }
+  // aniMoving 사용자 정의 메소드
+  $.fn.aniMoving = function () {
+    let elementTop = $(this).offset().top;
+    let elementBottom = elementTop + $(this).outerHeight();
+    let viewportTop = $(window).scrollTop();
+    let viewportBottom = viewportTop + $(window).height();
+    return (viewportTop < elementBottom) && (elementTop < viewportBottom);
+  };
 
   $(window).on('load scroll resize', function () {
-    ani.each(function () {
+    $('.ani').each(function () {
       if ($(this).aniMoving()) {
-        $(this).addClass('moving')
+        $(this).addClass('moving');
+      } else {
+        $(this).removeClass('moving');
       }
-      else {
-        $(this).removeClass('moving')
-      }
-    })
-  })
-
-  // 탭메뉴 함수
-  tap()
-  function tap(){
-    let i = 0
-    init(i)
-    function init(i)
-    {
-      $('.sec2_btn_wrap .mainBtn').eq(i).addClass('On')
-      $('.mainSec2 .img_box .ping').eq(i).addClass('On')
-    }
-    function reset(){
-      TAP.removeClass('On')
-      PING.removeClass('On')
-    }
-  
-    const TAP = $('.sec2_btn_wrap .mainBtn')
-    const PING = $('.mainSec2 .img_box .ping')
-  
-    TAP.click(function(){
-        let liIn = $(this).index()
-        reset()
-        $(this).addClass('On')
-        console.log(liIn)
-        init(liIn)
-    })
-  }
-
-  // 슬라이더
-  const swiper = new Swiper(".listSlider", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    breakpoints:{
-      768:{
-        slidesPerView: 3,
-        spaceBetween: -10,
-      },
-      450:{
-        slidesPerView: 1.5,
-      }
-    },
-    slidesPerView: 1,
-    loop: true,
-    spaceBetween: 0,
-    centeredSlides: true
+    });
   });
 
-})
+  function tap(){
+    const TAP = $('.sec2_btn_wrap .mainBtn');
+    const PING = $('.mainSec2 .img_box .ping');
+    if (TAP.length === 0) return; // 탭 메뉴가 없으면 종료
+
+    let i = 0;
+    init(i);
+
+    function init(i) {
+      TAP.eq(i).addClass('On');
+      PING.eq(i).addClass('On');
+    }
+    
+    function reset(){
+      TAP.removeClass('On');
+      PING.removeClass('On');
+    }
+  
+    TAP.click(function(){
+      let liIn = $(this).index();
+      reset();
+      init(liIn);
+    });
+  }
+
+  // [수정] Swiper 에러 방지 (요소가 있을 때만 실행)
+  if ($('.listSlider').length > 0) {
+    const swiper = new Swiper(".listSlider", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      breakpoints:{
+        768:{ slidesPerView: 3, spaceBetween: -10 },
+        450:{ slidesPerView: 1.5 }
+      },
+      slidesPerView: 1,
+      loop: true,
+      spaceBetween: 0,
+      centeredSlides: true
+    });
+  }
+
+});
